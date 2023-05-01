2e2d1d04bd959fe7b56c96389553679fa93b416b
bfl5322 <bfl5322@psu.edu>
2023-05-01 00:40:19 -0400

    fixed yes

--- src/bar-bar.js
+++ src/bar-bar.js
@@ -5,61 +5,71 @@ import { progressBarData } from './backend.js';
 class BarBar extends LitElement {
   static get properties() {
     return {
+      duration: { type: Number },
+      intervalDuration: { type: Number },
       progress: { type: Number },
       time: { type: Number },
-      start: { type: Number },
     };
   }
 
   static get styles() {
     return css`
-      .progress-bar {
-        background-color: #f3f3f3;
-        border-radius: 5px;
-        overflow: hidden;
-        position: relative;
-        width: 100%;
-        height: 25px;
-      }
-
-      .progress {
-        background-color: #4caf50;
-        height: 100%;
-        position: absolute;
-        transition: width 0.25s ease-out;
-        width: 0;
-      }
+:host {
+      display: block;
+      margin-bottom: 20px;
+    }
+    .progress-bar {
+      background-color: #ddd;
+      border-radius: 5px;
+      height: 30px;
+      width: 100%;
+    }
+    .progress-bar-inner {
+      background-color: #4caf50;
+      border-radius: 5px;
+      height: 100%;
+      width: 0;
+    }
+    .timer {
+      margin-top: 5px;
+      text-align: center;
+    }
     `;
   }
 
   constructor() {
     super();
+    this.duration = 10;
+    this.intervalDuration = 100;
     this.progress = 0;
     this.time = 0;
-    this.start = 0;
+  }
+  firstUpdated() {
+    this.startAnimation();
   }
 
-  connectedCallback() {
-    super.connectedCallback();
-    this.startCounting();
-  }
-
-  startCounting() {
+  startAnimation() {
+    const progressBarInner = this.shadowRoot.querySelector('.progress-bar-inner');
+    const timeIncrement = this.intervalDuration / 1000;
     const interval = setInterval(() => {
-      this.progress = Math.min(this.progress + this.start, this.time);
-      this.start++;
-      if (this.progress >= this.time) {
+      const progress = (this.time / this.duration) * 100;
+      progressBarInner.style.width = progress + '%';
+      this.time = parseFloat((this.time + timeIncrement).toFixed(1));
+
+      if (this.time >= this.duration) {
         clearInterval(interval);
       }
-    }, 1);
+
+      this.requestUpdate();
+    }, this.intervalDuration);
   }
 
   render() {
-    const percentage = (this.progress / this.time) * 100;
     return html`
       <div class="progress-bar">
-        <div class="progress" style="width: ${percentage}%"></div>
+        <div class="progress-bar-inner"></div>
       </div>
+      <div class="timer">${this.time}s</div>
     `;
   }
 }
