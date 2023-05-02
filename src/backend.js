4347e9823b038c5ecd6f96f1dc5a433bc601e370
bfl5322 <bfl5322@psu.edu>
2023-05-01 21:54:37 -0400

    fixed finally

--- src/backend.js
+++ src/backend.js
@@ -1,8 +1,6 @@
-
-
 export const progressBarData = [
-    { id: 1, time: 1000, start: 0 },
-    { id: 2, time: 2000, start: 0 },
-    { id: 3, time: 3000, start: 0 },
-  ];
+  { id: 1, duration: 1 },
+  { id: 2, duration: 20},
+  { id: 3, duration: 30},
+];
   
